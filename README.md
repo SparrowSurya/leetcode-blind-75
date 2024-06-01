5427515fda824ca84d3290453d87dd157b9c0add
sparrowsurya <thakur.suryakant.0406@gmail.com>
2024-06-01 19:18:00 +0530

    two-sum.cpp

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 [leetcode](https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions) blind 75 questions.
 
 ## Questions:
-1. [two-sum](https://leetcode.com/problems/two-sum/) [code](./two-sum.py)
+1. [two-sum](https://leetcode.com/problems/two-sum/) | [py](./two-sum.py) | [c++](./two-sum.cpp)
---
+++ two-sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+using namespace std;
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> hashmap;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      int diff = target - nums[i];
+      if (hashmap.count(diff)) {
+        return {hashmap[diff], i};
+      }
+      hashmap[nums[i]] = i;
+    }
+
+    return {};
+  }
+};
